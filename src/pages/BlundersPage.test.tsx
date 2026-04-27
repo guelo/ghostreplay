d7201204a2c42451d12fe2acb16153d0e8d66055
miguel <unroar@gmail.com>
2026-04-26 22:17:08 -0700

    Fix BlundersPage test build

--- src/pages/BlundersPage.test.tsx
+++ src/pages/BlundersPage.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach, beforeAll } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import BlundersPage from './BlundersPage';
 
