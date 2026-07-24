c1506590e7cdcd34eef4bc4078a0214120358a6d
miguel <unroar@gmail.com>
2026-07-24 02:44:39 -0700

    misc

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,28 +1,31 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
 import AuthForm from "./components/AuthForm";
-import BlundersPage from "./pages/BlundersPage";
-import GameAnalysisPage from "./pages/GameAnalysisPage";
-import DrillAnalysisPage from "./pages/DrillAnalysisPage";
-import HistoryPage from "./pages/HistoryPage";
-import GamePage from "./pages/GamePage";
-import OpeningsPage from "./pages/OpeningsPage";
-import StatsPage from "./pages/StatsPage";
+const BlundersPage = lazy(() => import("./pages/BlundersPage"));
+const GameAnalysisPage = lazy(() => import("./pages/GameAnalysisPage"));
+const DrillAnalysisPage = lazy(() => import("./pages/DrillAnalysisPage"));
+const HistoryPage = lazy(() => import("./pages/HistoryPage"));
+const GamePage = lazy(() => import("./pages/GamePage"));
+const OpeningsPage = lazy(() => import("./pages/OpeningsPage"));
+const StatsPage = lazy(() => import("./pages/StatsPage"));
 
 function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/play" element={<GamePage />} />
-      <Route path="/game" element={<GameAnalysisPage />} />
-      <Route path="/drill-analysis" element={<DrillAnalysisPage />} />
-      <Route path="/login" element={<AuthForm mode="login" />} />
-      <Route path="/register" element={<AuthForm mode="register" />} />
-      <Route path="/history" element={<HistoryPage />} />
-      <Route path="/blunders" element={<BlundersPage />} />
-      <Route path="/openings" element={<OpeningsPage />} />
-      <Route path="/stats" element={<StatsPage />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/play" element={<GamePage />} />
+        <Route path="/game" element={<GameAnalysisPage />} />
+        <Route path="/drill-analysis" element={<DrillAnalysisPage />} />
+        <Route path="/login" element={<AuthForm mode="login" />} />
+        <Route path="/register" element={<AuthForm mode="register" />} />
+        <Route path="/history" element={<HistoryPage />} />
+        <Route path="/blunders" element={<BlundersPage />} />
+        <Route path="/openings" element={<OpeningsPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
